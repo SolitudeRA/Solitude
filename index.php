f170b265f1ef263f845827ae4c9f351f284d44ec
Luna <arthu.lee.ac72@outlook.com>
2017-08-17 17:09:11 +0800

    update content font

--- index.php
+++ index.php
@@ -2,24 +2,24 @@
 
 <?php get_sidebar(); ?>
 
-    <div class="posts-container">
-        <?php if ( have_posts() ) :
-            
-            while ( have_posts() ) : the_post();
-                
-                get_template_part( "template-parts/post/content", get_post_format() );
-            
+    <div class="container-full posts-container">
+        <?php if (have_posts()) :
+
+            while (have_posts()) : the_post();
+
+                get_template_part("template-parts/post/content", get_post_format());
+
             endwhile;
-            
-            the_posts_pagination( array(
-                "mid_size"           => "",
-                "prev_text"          => "",
-                "next_text"          => "",
+
+            the_posts_pagination(array(
+                "mid_size" => "",
+                "prev_text" => "",
+                "next_text" => "",
                 "before_page_number" => ""
-            ) );
-        
+            ));
+
         else : get_404_template();
-        
+
         endif;
         ?>
     </div>
--- template-parts/post/content.php
+++ template-parts/post/content.php
@@ -1,43 +1,39 @@
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class("row container-full col-iPad-12 col-iPadPro-12 col-laptop-12 col-fhd-12 col-wqhd-12", null); ?>>
 
     <header class="post_header">
-        <?php if ( "" !== get_the_post_thumbnail() && ! is_single() ): ?>
+        <?php if ("" !== get_the_post_thumbnail() && !is_single()): ?>
             <div class="post-thumbnail">
-                <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail( "" ); ?></a>
+                <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(""); ?></a>
             </div><!-- Post thumbnail -->
         <?php endif; ?>
         <div class="post_title">
-            <?php if ( is_single() ):
-                the_title( "", "" );
+            <?php if (is_single()):
+                the_title(null, null);
             else:
-                the_title( "", "" );
+                the_title(null, null);
             endif; ?>
         </div>
         <div class="post_meta">
             <section class="post_meta_time">
-                <h4><?php _e( "Time posted:", "galaxy-domain" ) ?></h4>
-                <?php the_date( "Y F j" ); ?><?php the_time( "g:i a" ); ?>
+                <h4><?php _e("Time posted:", "galaxy-domain") ?></h4>
+                <?php the_date("Y F j"); ?>  <?php the_time("g:i a"); ?>
             </section>
             <section class="post_meta_cat">
-                <h4><?php _e( "Category", "galaxy-domain" ) ?></h4>
-                <?php the_category( " " ); ?>
+                <h4><?php _e("Category", "galaxy-domain") ?></h4>
+                <?php the_category(" "); ?>
             </section>
             <section class="post_meta_tags">
-                <h4><?php _e( "Tags", "galaxy-domain" ) ?></h4>
-                <?php the_tags( "", ",", "" ); ?>
+                <h4><?php _e("Tags", "galaxy-domain") ?></h4>
+                <?php the_tags(null, ",", null); ?>
             </section>
             <section class="post_meta_edit">
-                <?php edit_post_link( __( 'Edit', 'galaxy-domain' ), "", "", null, "" ) ?>
+                <?php edit_post_link(__('Edit', 'galaxy-domain'), null, null, null, null) ?>
             </section>
         </div>
     </header>
 
     <div class="post_content">
-        <?php the_content( sprintf( __( "Read more...", "galaxy-domain" ), get_the_title() ) ); ?>
+        <?php the_content(sprintf(__("Read more...", "galaxy-domain"), get_the_title())); ?>
     </div>
-    
-    <?php the_post_navigation( array(
-        'next_text' => '<span class="post_nav_title">%title</span>',
-        'prev_text' => '<span class="post_nav_title">%title</span>'
-    ) ); ?>
+
 </article>
