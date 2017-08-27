590f8a24ceee1a6372563e4ab0beda5990a2614d
Arthur.Lee <arthur.lee.ac72@gmail.com>
2017-08-27 23:30:36 +0800

    test update

--- index.php
+++ index.php
@@ -1,30 +1,31 @@
 <?php get_header(); ?>
 
-<div class="container-full">
-    <?php get_sidebar("left"); ?>
+<div class="container-full main-container">
+    <?php get_sidebar( "left" ); ?>
+    <div class="row">
+        <div class="posts-container">
+            <?php if ( have_posts() ) :
 
-    <div class="posts-container">
-        <?php if(have_posts()) :
+                while ( have_posts() ) : the_post();
 
-            while(have_posts()) : the_post();
+                    get_template_part( "template-parts/post/content", get_post_format() );
 
-                get_template_part("template-parts/post/content", get_post_format());
+                endwhile;
 
-            endwhile;
+                the_posts_pagination( array(
+                    "mid_size"           => "",
+                    "prev_text"          => "",
+                    "next_text"          => "",
+                    "before_page_number" => ""
+                ) );
 
-            the_posts_pagination(array(
-                                     "mid_size"           => "",
-                                     "prev_text"          => "",
-                                     "next_text"          => "",
-                                     "before_page_number" => ""
-                                 ));
+            else : get_404_template();
 
-        else : get_404_template();
-
-        endif;
-        ?>
+            endif;
+            ?>
+        </div>
+        <?php get_sidebar( "right" ) ?>
     </div>
-
 </div>
 <?php get_footer(); ?>
 
--- template-parts/post/content.php
+++ template-parts/post/content.php
@@ -1,39 +1,39 @@
-<article id="post-<?php the_ID(); ?>" <?php post_class("container-full", null); ?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class( "container-full", null ); ?>>
 
-    <header class="post_header">
-        <?php if("" !== get_the_post_thumbnail()): ?>
+    <header class="post-header">
+        <?php if ( "" !== get_the_post_thumbnail() ): ?>
             <div class="row post-thumbnail">
-                <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail(""); ?></a>
+                <a href="<?php the_permalink(); ?>"><?php the_post_thumbnail( "" ); ?></a>
             </div><!-- Post thumbnail -->
         <?php endif; ?>
-        <div class="row post_title_container">
-            <?php if(is_single()):
-                the_title("<div class='post_title_single'>", "</div>");
+        <div class="row post-title-container">
+            <?php if ( is_single() ):
+                the_title( "<div class='post_title_single'>", "</div>" );
             else:
-                the_title("<div class='post_title_index'>", "</div>");
+                the_title( "<div class='post_title_index'>", "</div>" );
             endif; ?>
         </div>
-        <div class="row post_meta_container">
+        <div class="row post-meta-container">
             <section class="post_meta_time">
-                <h4><?php _e("Time posted:", "galaxy-domain") ?></h4>
-                <?php the_date("Y F j"); ?>  <?php the_time("g:i a"); ?>
+                <h4><?php _e( "Time posted:", "galaxy-domain" ) ?></h4>
+                <?php the_date( "Y F j" ); ?>  <?php the_time( "g:i a" ); ?>
             </section>
-            <section class="post_meta_cat">
-                <h4><?php _e("Category", "galaxy-domain") ?></h4>
-                <?php the_category(" "); ?>
+            <section class="post-meta-cat">
+                <h4><?php _e( "Category", "galaxy-domain" ) ?></h4>
+                <?php the_category( " " ); ?>
             </section>
-            <section class="post_meta_tags">
-                <h4><?php _e("Tags", "galaxy-domain") ?></h4>
-                <?php the_tags(null, ",", null); ?>
+            <section class="post-meta-tags">
+                <h4><?php _e( "Tags", "galaxy-domain" ) ?></h4>
+                <?php the_tags( null, ",", null ); ?>
             </section>
-            <section class="post_meta_edit">
-                <?php edit_post_link(__('Edit', 'galaxy-domain'), null, null, null, null) ?>
+            <section class="post-meta-edit">
+                <?php edit_post_link( __( 'Edit', 'galaxy-domain' ), null, null, null, null ) ?>
             </section>
         </div>
     </header>
 
-    <div class="row post_content">
-        <?php the_content(sprintf(__("Read more...", "galaxy-domain"), get_the_title())); ?>
+    <div class="row post-content">
+        <?php the_content( sprintf( __( "Read more...", "galaxy-domain" ), get_the_title() ) ); ?>
     </div>
 
 </article>
