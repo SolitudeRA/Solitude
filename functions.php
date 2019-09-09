33605743a06b6a1c919c5a48a7e8895a51f87a61
SolitudeRA <solitude@protogalaxy.me>
2019-09-10 01:30:57 +0900

    functions update

--- functions.php
+++ functions.php
@@ -1,7 +1,16 @@
 <?php
+/**
+ * Solitude functions and definitions
+ *
+ * @link
+ *
+ * @package Solitude
+ * @since 1.0
+ */
+
 
 /**
- * Solitude only works in WordPress 4.7 or later.
+ * Solitude only works in WordPress 5.2 or later.
  */
 if ( version_compare( $GLOBALS['wp_version'], '5.2', '<' ) ) {
 	require get_template_directory() . '/inc/back-compat.php';
@@ -17,6 +26,23 @@ if ( version_compare( $GLOBALS['wp_version'], '5.2', '<' ) ) {
  * as indicating support for post thumbnails.
  */
 function solitude_setup() {
+	/*
+    * Make theme available for translation.
+    */
+	load_theme_textdomain( 'solitude' );
+
+	/*
+	 * Switch default core markup for search form, comment form, and comments
+	 * to output valid HTML5.
+	 */
+	add_theme_support( "html5", array(
+		"search-form",
+		"comment-form",
+		"comment-list",
+		"gallery",
+		"caption"
+	) );
+
 	// Add theme support for Custom Logo.
 	add_theme_support( "custom-logo", array(
 		"width"       => 250,
@@ -26,17 +52,17 @@ function solitude_setup() {
 	) );
 
 	/*
-    * Enable support for Post Thumbnails on posts and pages.
-    *
-    * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
-    */
+	* Enable support for Post Thumbnails on posts and pages.
+	*
+	* @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+	*/
 	add_theme_support( 'post-thumbnails' );
 
 	/*
-    * Enable support for Post Formats.
-    *
-    * See: https://codex.wordpress.org/Post_Formats
-    */
+	* Enable support for Post Formats.
+	*
+	* See: https://codex.wordpress.org/Post_Formats
+	*/
 	add_theme_support( "post-formats", array(
 		"aside",
 		"audio",
@@ -47,6 +73,15 @@ function solitude_setup() {
 		"quote",
 		"status"
 	) );
+
+	// Add default posts and comments RSS feed links to head.
+	add_theme_support( 'automatic-feed-links' );
+
+	// Add theme support for selective refresh for widgets.
+	add_theme_support( 'customize-selective-refresh-widgets' );
+
+	// Set the default content width.
+	$GLOBALS['content_width'] = 525;
 
 	// This theme uses wp_nav_menu() in three locations.
 	register_nav_menus(
@@ -71,98 +106,93 @@ function solitude_setup() {
 		"admin-preview-callback" => ""
 	) );
 
-	/*
-	 * Switch default core markup for search form, comment form, and comments
-	 * to output valid HTML5.
-	 */
-	add_theme_support( "html5", array(
-		"search-form",
-		"comment-form",
-		"comment-list",
-		"gallery",
-		"caption"
-	) );
-
 	add_theme_support( "post-thumbnails" );
 
 	//Add post thumbnails sizes
-	add_image_size( "full-size", 9999, 9999 );
-	add_image_size( "post-size", "", "" );
-	add_image_size( "avatar-large-size", "", "" );
-	add_image_size( "avatar-medium-size", "", "" );
-	add_image_size( "avatar-small-size", "", "" );
+	add_image_size( "wqhd-size", 2560, 1440 );
+	add_image_size( "fhd-size", 1920, 1080 );
+	add_image_size( "hd-size", 1280, 720 );
+	add_image_size( "avatar-large-size", 500, 500 );
+	add_image_size( "avatar-medium-size", 250, 250 );
+	add_image_size( "avatar-small-size", 125, 125 );
 
 	//Loads the theme's translated strings
-	load_theme_textdomain( "galaxy-domain", get_template_directory() . "/languages" );
+	load_theme_textdomain( "solitude-domain", get_template_directory() . "/languages" );
 }
 
-add_action( "after_setup_theme", "galaxy_setup" );
+add_action( "after_setup_theme", "solitude_setup" );
 
-function galaxy_scripts() {
-	wp_enqueue_style( "galaxy-fonts-zh", "fonts.googleapis.com/earlyaccess/notosanssc.css" );
-	wp_enqueue_style( "galaxy-fonts-zh-sliced", "fonts.googleapis.com/earlyaccess/notosansscsliced.css" );
-	wp_enqueue_style( "galaxy-fonts-jp", "fonts.googleapis.com/earlyaccess/notosansjp.css" );
-	wp_enqueue_style( "galaxy-fonts-en", "fonts.googleapis.com/css?family=Roboto:300,400,500,700" );
-	wp_enqueue_style( "font-awesome", "assets/engine/font-awesome/css/font-awesome.min.css" );
-	wp_enqueue_style( "galaxy-style", get_stylesheet_uri() );
-	wp_enqueue_script( "jQuery", "assets/engine/jQuery/jquery-3.2.1.min.js", "jQuery", "3.2.1", true );
-	wp_enqueue_script( "galaxy-global", "assets/js/galaxy-global.js", "Global-Script", "1.0.0", true );
-}
-
-add_action( "wp_enqueue_scripts", "galaxy_scripts" );
-
-function galaxy_widgets_init() {
+function solitude_widgets_init() {
 	register_sidebar( array(
-		"name"          => __( "Sidebar left", "galaxy-domain" ),
+		"name"          => __( "Sidebar left", "solitude-domain" ),
 		"id"            => "sidebar-left",
-		"description"   => __( "Add widgets here to appear in your left sidebar.", "galaxy-domain" ),
+		"description"   => __( "Add widgets here to appear in your left sidebar.", "solitude-domain" ),
 		"class"         => "",
 		"before_widget" => "<li class='widget'>",
 		"after_widget"  => "</li>",
-		"before_title"  => "<h2 class='galaxy-widget-title'>",
+		"before_title"  => "<h2 class='sidebar-widget-title'>",
 		"after_title"   => "</h2>"
 	) );
 
 	register_sidebar( array(
-		"name"          => __( "Sidebar right", "galaxy-domain" ),
+		"name"          => __( "Sidebar right", "solitude-domain" ),
 		"id"            => "sidebar-right",
-		"description"   => __( "Add widgets here to appear in your right sidebar.", "galaxy-domain" ),
+		"description"   => __( "Add widgets here to appear in your right sidebar.", "solitude-domain" ),
 		"class"         => "",
 		"before_widget" => "<li class='widget'>",
 		"after_widget"  => "</li>",
-		"before_title"  => "<h2 class='galaxy-widget-title'>",
+		"before_title"  => "<h2 class='sidebar-widget-title'>",
 		"after_title"   => "</h2>"
 	) );
 
 	register_sidebar( array(
-		"name"          => __( "Sidebar header", "galaxy-domain" ),
+		"name"          => __( "Sidebar header", "solitude-domain" ),
 		"id"            => "sidebar-header",
-		"description"   => __( "Add widgets here to appear in your header.", "galaxy-domain" ),
+		"description"   => __( "Add widgets here to appear in your header.", "solitude-domain" ),
 		"class"         => "",
 		"before_widget" => "<li class='widget'>",
 		"after_widget"  => "</li>",
-		"before_title"  => "<h2 class='galaxy-widget-title'>",
+		"before_title"  => "<h2 class='solitude-widget-title'>",
 		"after_title"   => "</h2>"
 	) );
 
 	register_sidebar( array(
-		"name"          => __( "Sidebar footer", "galaxy-domain" ),
+		"name"          => __( "Sidebar footer", "solitude-domain" ),
 		"id"            => "sidebar-footer",
-		"description"   => __( "Add widgets here to appear in your footer.", "galaxy-domain" ),
+		"description"   => __( "Add widgets here to appear in your footer.", "solitude-domain" ),
 		"class"         => "",
 		"before_widget" => "<li class='widget'>",
 		"after_widget"  => "</li>",
-		"before_title"  => "<h2 class='galaxy-widget-title'>",
+		"before_title"  => "<h2 class='solitude-widget-title'>",
 		"after_title"   => "</h2>"
 	) );
 }
 
-add_action( "widgets_init", "galaxy_widgets_init" );
+add_action( "widgets_init", "solitude_widgets_init" );
 
-function galaxy_customize_register( $wp_customize ) {
+function solitude_customize_register( $wp_customize ) {
 	$wp_customize->add_section( "color_theme_section", array(
-		'title' => __( 'Color theme', "galaxy-domain" )
+		'title' => __( 'Color theme', "solitude-domain" )
 	) );
 }
 
-add_action( "customise_register", "galaxy_customise_register" );
+add_action( "customise_register", "solitude_customise_register" );
+
+/**
+ * Enqueues scripts and styles.
+ */
+function solitude_scripts() {
+	if ( 'light' === get_theme_mod( 'color_scheme', 'dark' ) ) {
+		wp_enqueue_style( "solitude-theme-light", "" );
+	}
+	wp_enqueue_style( "solitude-fonts-zh", "fonts.googleapis.com/earlyaccess/notosanssc.css" );
+	wp_enqueue_style( "solitude-fonts-zh-sliced", "fonts.googleapis.com/earlyaccess/notosansscsliced.css" );
+	wp_enqueue_style( "solitude-fonts-jp", "fonts.googleapis.com/earlyaccess/notosansjp.css" );
+	wp_enqueue_style( "solitude-fonts-en", "fonts.googleapis.com/css?family=Roboto:300,400,500,700" );
+	wp_enqueue_style( "font-awesome", "assets/engine/font-awesome/css/font-awesome.min.css" );
+	wp_enqueue_style( "solitude-style", get_stylesheet_uri() );
+	wp_enqueue_script( "jQuery", "assets/engine/jQuery/jquery-3.2.1.min.js", "jQuery", "3.2.1", true );
+	wp_enqueue_script( "solitude-global", "assets/js/solitude-global.js", "Global-Script", "1.0.0", true );
+}
+
+add_action( "wp_enqueue_scripts", "solitude_scripts" );
